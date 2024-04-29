33de59006b3f1d56f9451fb80bd81b1670dc9063
profe-ggarrido <profe_ggarrido@hotmail.com>
2024-04-28 21:10:07 -0300

    Desaf1-Proyecto-Django

---
+++ laesquina/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\profe\OneDrive\Documentos\DESARROLLO-WEB-SENCE\Modulo 6 - Django\Seccion 1 Intro\Desaf Entornos Viirtuales\laesquina\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
